5ccc8b6f3d43d42dd52f4893ef71c1dd67ca7104
Charliemur2 <charliemurciamunoz@gmail.com>
2020-02-12 04:41:37 +0000

    create 7-print_last_digit.c and holberton.h update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@
 
 int print_last_digit(int r)
 {
-	if (r <= 0)
+	if (r >= 0)
 	{
-		_putchar((r * (-1) % 10) + '0');
+		_putchar((r % 10) + '0');
 	}
 	else
 	{
-		_putchar((r % 10) + '0');
+		_putchar((r * (-1) % 10) + '0');
 	}
 	return (r);
 }
