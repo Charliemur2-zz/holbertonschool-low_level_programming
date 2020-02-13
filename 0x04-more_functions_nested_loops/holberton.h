648e416b5a48e7517b26d893f7f9b9b595f361fb
Charliemur2 <charliemurciamunoz@gmail.com>
2020-02-13 16:13:26 +0000

    create 0x04-more_functions_nested_loops, 0-isupper.c 1-isdigit.c 2-mul.c and readme file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * my header for 0x02 functions and nested loops
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
+#endif /* _HOLBERTON_H_ */
