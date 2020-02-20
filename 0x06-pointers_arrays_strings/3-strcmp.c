413bc4809615cf0ccf26fdd3107aad4840bf92a4
Charliemur2 <charliemurciamunoz@gmail.com>
2020-02-20 22:33:16 +0000

    create 3-strcmp.c and holberton update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,32 +4,18 @@
  * _strcmp - compare 2 strings
  * @s1: first string
  * @s2: second string
- * Return: 15. -15 or 0
+ * Return: temp
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1len = 0;
-	int s2len = 0;
+	int i = 0;
+	int temp;
 
-	while (s1[s1len] != '\0')
+	while (s1[i] == s2[i] && s2[i] != '\0' && s1[i] != '\0')
 	{
-		s1len++;
+		i++;
 	}
-	while (s2[s2len] != '\0')
-	{
-		s2len++;
-	}
-	if (s1len > s2len)
-	{
-		return (15);
-	}
-	else if (s1len == s2len)
-	{
-		return (0);
-	}
-	else
-	{
-		return (-15);
-	}
+	temp = s1[i] - s2[i];
+	return (temp);
 }
