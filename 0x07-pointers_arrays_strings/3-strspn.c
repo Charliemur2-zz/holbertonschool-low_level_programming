01da9ec1ca95bb64fa989edd0eed186d067169d7
Charliemur2 <charliemurciamunoz@gmail.com>
2020-02-24 21:46:33 +0000

    create 3-strspn.c and holberton update

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strspn - Returns the number of bytes in the initial segment
+ * @s: string s
+ * @accept: take this bytes
+ * Return: n
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int n;
+	int i;
+	int j;
+
+	n = 0;
+	i = 0;
+	while (s[i] != ' ')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				n++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (n);
+}
