d0d6af76c672bf94a8e260791d40d58d0bc0ae97
Charliemur2 <charliemurciamunoz@gmail.com>
2020-03-10 03:55:21 +0000

    2-print_dog.c and dig,h update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,13 @@ void print_dog(struct dog *d)
 	{
 		if ((*d).name == NULL)
 			printf("Name: (nil)");
-		if ((*d).owner == NULL)
+		else if ((*d).owner == NULL)
 			printf("Name: (nil)");
+		else
+		{
 		printf("Name: %s\n", (*d).name);
 		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
+		}
 	}
 }
