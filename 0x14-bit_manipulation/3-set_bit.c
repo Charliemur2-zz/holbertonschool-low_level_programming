47e14e5b356b80bf6fc614ba110d4d516bdb5ab4
Charliemur2 <charliemurciamunoz@gmail.com>
2020-03-28 01:40:31 +0000

    3-set_bit.c and update holberton.h

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,6 +4,7 @@
  * set_bit - set a bit in the index position
  * @n: pointer to binary number
  * @index: position
+ * Return: 1 if succes or -1 if not
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
