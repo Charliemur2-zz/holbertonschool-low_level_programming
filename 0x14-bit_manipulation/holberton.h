76e7de685fdfe168863c78989a326042b8d8f03c
Charliemur2 <charliemurciamunoz@gmail.com>
2020-03-28 01:23:12 +0000

    create 0x14-bit_manipulation 1-print_binary.c holberton.h

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif /* _HOLBERTON_H_ */
