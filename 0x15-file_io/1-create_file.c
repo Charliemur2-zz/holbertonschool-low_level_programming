d7a090ba50591189218759ed7e23bad07b11fffa
Charliemur2 <charliemurciamunoz@gmail.com>
2020-04-01 02:18:08 +0000

    create 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,15 +13,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[len])
-		len++;
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+       	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 	{
 		return (-1);
 	}
 	if (text_content == NULL)
 		return (1);
+	while (text_content[len])
+		len++;
 	fdw = write(fd, text_content, len);
 	if (fdw < 0)
 	{
