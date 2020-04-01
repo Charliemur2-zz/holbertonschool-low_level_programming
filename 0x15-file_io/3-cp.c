b1300c278feabfaf166806ef7a6fabb4ecf60e75
Charliemur2 <charliemurciamunoz@gmail.com>
2020-04-01 05:38:31 +0000

    create 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,37 +14,40 @@
 
 int main(int argc, char *argv[])
 {
-	int fdo1, fdo2, fdr, fdw, cl, cl2;
-	char arr[1024];
+	int fdo1, fdo2, fdr, cl, cl2;
+	char arr[1024], e1[] = "Error: Can't read from file";
+	char e2[] = "Error: Can't write from file";
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fdo1 = open(argv[1], O_RDONLY);
 	if (fdo1 < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "%s %s\n", e1, argv[1]);
 		exit(98);
 	}
 	fdo2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fdo2 < 0)
 	{
-		dprintf(2, "Error: Can't write from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "%s %s\n", e2, argv[2]);
 		exit(99);
 	}
-	fdr = read(fdo1, arr, 1024);
-	if (fdr < 0)
-		return (-1);
-	fdw = write(fdo2, arr, fdr);
-	if (fdw < 0)
-		return (-1);
+	while ((fdr = read(fdo1, arr, 1024)) > 0)
+	{
+		if (write(fdo2, arr, fdr) != fdr)
+		{
+			dprintf(STDERR_FILENO, "%s %s\n",e2, argv[2]);
+			exit(98);
+		}
+	}
 	cl = close(fdo1);
 	cl2 = close(fdo2);
 	if ((cl == -1) || (cl2 == -1))
 	{
-		dprintf(2, "Error: Can't close fd %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
 		exit(100);
 	}
 	return (0);
