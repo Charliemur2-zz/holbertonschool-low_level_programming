8cf8124a693f0343e898e77e6b12c664d96425da
Charliemur2 <charliemurciamunoz@gmail.com>
2020-05-29 16:54:06 +0000

    create 3-hash_table_set.c and update hash_table.h

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,20 +11,20 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *ht_node, *ht_ptr;
+	hash_node_t *ht_node;
 
 	if (!ht || !key || !value || strlen(key) == 0)
 		return (0);
 	index = key_index((unsigned char *) key, ht->size);
-	ht_ptr = (ht->array)[index];
-	while (ht_ptr != NULL)
+	ht_node = (ht->array)[index];
+	while (ht_node != NULL)
 	{
-		if (strcmp(ht_ptr->key, key) == 0)
+		if (strcmp(ht_node->key, key) == 0)
 		{
-			ht_ptr->value = strdup(value);
+			ht_node->value = strdup(value);
 			return (1);
 		}
-		ht_ptr = ht_ptr->next;
+		ht_node = ht_node->next;
 	}
 	ht_node = malloc(sizeof(hash_node_t));
 	if (ht_node == NULL)
@@ -32,7 +32,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht_node->key = strdup(key);
 	ht_node->value = strdup(value);
 	ht_node->next = NULL;
-	ht_ptr = (ht->array)[index];
-	ht_ptr = ht_node;
 	return (1);
 }
