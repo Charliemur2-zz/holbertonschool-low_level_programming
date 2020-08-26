0c6bfcb58fb1e0ccaac95e19c99363e923f2bb6b
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-26 22:31:22 +0000

    create search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+#include <stdlib.h>
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int linear_search(int *array, size_t size, int value);
+#endif /* _SEARCH_ALGOS_H_ */
